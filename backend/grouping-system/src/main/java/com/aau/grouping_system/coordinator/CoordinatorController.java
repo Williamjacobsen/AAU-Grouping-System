ee8c2157ceeb1476fb0b7a0e2c3e6c271f2c56f7
Williamjacobsen <villi05.v.j@gmail.com>
2025-09-25 13:56:35 +0200

    backend setup + in-memory database + coordinator example

---
+++ backend/grouping-system/src/main/java/com/aau/grouping_system/coordinator/CoordinatorController.java
@@ -0,0 +1,33 @@
+package com.aau.grouping_system.coordinator;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aau.grouping_system.database.Database;
+
+@RestController // singleton bean
+@RequestMapping("/coordinator")
+public class CoordinatorController {
+    private final Database db;
+
+    // dependency injection
+    public CoordinatorController(Database db) {
+        this.db = db;
+    }
+    
+    @GetMapping
+    public Map<Integer, String> getSessions() {
+        return db.getAllSessions();
+    }
+
+    // How to test: send a post request "http://localhost:8080/coordinator/createSession?sessionName=test"
+    @PostMapping("/createSession")
+    public void createSession(@RequestParam String sessionName) {
+        db.saveSession(sessionName);
+    }
+}
---
+++ backend/grouping-system/src/main/java/com/aau/grouping_system/database/Database.java
@@ -0,0 +1,30 @@
+package com.aau.grouping_system.database;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.stereotype.Component;
+
+@Component // so we can do dependency injection
+public class Database {
+   private final Map<Integer, String> sessions = new ConcurrentHashMap<>();
+   private final AtomicInteger idGenerator = new AtomicInteger();
+
+    public void saveSession(String data) {
+        int id = idGenerator.incrementAndGet();
+        sessions.put(id, data);
+    }
+
+    public String getSession(Integer id) {
+        return sessions.get(id);
+    }
+
+    public Map<Integer, String> getAllSessions() {
+        return sessions;
+    }
+
+    public void deleteSession(Integer id) {
+        sessions.remove(id);
+    } 
+}
