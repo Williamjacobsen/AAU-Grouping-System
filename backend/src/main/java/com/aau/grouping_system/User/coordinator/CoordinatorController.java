8a789790be15298a54eb2dea28d72a04e03decc4
Jesp <87063030+JespJesp@users.noreply.github.com>
2025-09-30 13:28:28 +0200

    Added duplicate email check, password hashing, and a databaseID field to Coordinator

--- backend/src/main/java/com/aau/grouping_system/User/coordinator/CoordinatorController.java
+++ backend/src/main/java/com/aau/grouping_system/User/coordinator/CoordinatorController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.aau.grouping_system.Database.Database;
 
 @RestController // singleton bean
@@ -12,36 +17,63 @@ import com.aau.grouping_system.Database.Database;
 public class CoordinatorController {
 
 	private final Database db;
+	private final PasswordEncoder passwordEncoder;
 
-	public CoordinatorController(Database db) {
+	public CoordinatorController(Database db, PasswordEncoder passwordEncoder) {
 		this.db = db;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	// requests
 
 	private record SignUpRequest(String email, String password, String name) { }
 	@PostMapping("/signUp")
-	public void signUp(@RequestBody SignUpRequest request) {
-		// todo: check that no other coordinator has the same email
-		// todo: hash password
-		String passwordHash = request.password();
+	public ResponseEntity<String> signUp(@RequestBody SignUpRequest request) {
+		if (isEmailDuplicate(request.email())) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT)
+      		.body("Error: Inputted email is already used by another coordinator.");
+		}
+		String passwordHash = passwordEncoder.encode(request.password());
 		Coordinator newCoordinator = new Coordinator(request.email(), passwordHash, request.name());
 		db.saveCoordinator(newCoordinator);
+		System.out.println("Added profile: " + "Email: " + request.email() + ", Password hash:" + passwordHash);
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+      .body("Coordinator has been added to database.");
 	}
 
 	private record ModifyEmailRequest(Coordinator coordinator, String newEmail) { }
 	@PostMapping("/modifyEmail")
-	public void modifyEmail(@RequestBody ModifyEmailRequest request) {
+	public ResponseEntity<String> modifyEmail(@RequestBody ModifyEmailRequest request) {
 		// todo: Credentials validation
+		if (isEmailDuplicate(request.newEmail())) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT)
+      		.body("Error: Inputted email is already used by another coordinator.");
+		}
 		request.coordinator().setEmail(request.newEmail());
+		return ResponseEntity
+			.status(HttpStatus.OK)
+      .body("Coordinator has been added to database.");
 	}
 
 	private record ModifyPasswordRequest(Coordinator coordinator, String newPassword) { }
 	@PostMapping("/modifyPassword")
 	public void modifyPassword(@RequestBody ModifyPasswordRequest request) {
 		// todo: Credentials validation
-		// todo: Hash password
-		String passwordHash = request.newPassword();
+		String passwordHash = passwordEncoder.encode(request.newPassword());
 		request.coordinator().setPasswordHash(passwordHash);
+	}
+
+	// helpers
+
+	private boolean isEmailDuplicate(String email) {
+		for (Coordinator existingCoordinator : db.getAllCoordinators().values()) {
+			if (existingCoordinator.getEmail().equals(email)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
