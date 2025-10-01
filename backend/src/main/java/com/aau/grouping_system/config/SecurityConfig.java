50ab95056d5a4ff4a11fdb5d312acc4c894a130e
MunasarMohamed <mmam24@student.aau.dk>
2025-10-01 09:14:05 +0200

    Login/out

--- backend/src/main/java/com/aau/grouping_system/config/SecurityConfig.java
+++ backend/src/main/java/com/aau/grouping_system/config/SecurityConfig.java
@@ -2,19 +2,6 @@ package com.aau.grouping_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< Updated upstream
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class SecurityConfig {
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(10); // You can adjust the strength (4-31), default is 10
-	}
-}
-=======
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,4 +16,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
->>>>>>> Stashed changes
