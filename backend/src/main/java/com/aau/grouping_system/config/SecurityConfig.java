4b6019a94ad75e9828946a419516e079a5a5f046
MunasarMohamed <mmam24@student.aau.dk>
2025-10-01 09:11:53 +0200

    Login/out

--- backend/src/main/java/com/aau/grouping_system/config/SecurityConfig.java
+++ backend/src/main/java/com/aau/grouping_system/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.aau.grouping_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+<<<<<<< Updated upstream
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,3 +14,19 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder(10); // You can adjust the strength (4-31), default is 10
 	}
 }
+=======
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// test
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())  // disable CSRF
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // allow all
+        return http.build();
+    }
+}
+>>>>>>> Stashed changes
