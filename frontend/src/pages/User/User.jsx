873ada817aa1af526b6c93c119561a76b3a90f52
MunasarMohamed <mmam24@student.aau.dk>
2025-10-10 09:54:34 +0200

    CSS added to sign-in/sign-up page

--- frontend/src/pages/User/User.jsx
+++ frontend/src/pages/User/User.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import "./User.css";
 
 export default function User() {
 	return (
-		<div>
-			This has not been implemented yet.
+
+		<div className="container"> 
+			<div className="header-text">Sign-in</div>
+			<div className="text"> 
+				<div className="input">
+					<label className="label"> Email
+						<input type="email" placeholder = "John123@email.com"/>
+					</label>
+				</div>
+				<div className="input">
+					<label className="label"> Password:
+						<input type="password" placeholder = "******" />
+					</label>
+				</div>
+			</div>
+			<div className="submit-container">
+				<div className="log-in"> Log In
+				</div>
+				<div className="sign-up"> Sign Up
+				</div>
+				<div className="forgot-password"> Forgot password?
+				</div>
+			</div>
 		</div>
+
+
 	);
 }
