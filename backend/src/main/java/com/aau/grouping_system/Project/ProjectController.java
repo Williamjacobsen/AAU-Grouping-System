7e0b386db7d7df5b54f7ed9b2acda97fbb80e029
kongemagnus <mklitg24@student.aau.dk>
2025-10-13 14:44:56 +0200

    Update ProjectController.java

--- backend/src/main/java/com/aau/grouping_system/Project/ProjectController.java
+++ backend/src/main/java/com/aau/grouping_system/Project/ProjectController.java
@@ -5,17 +5,12 @@ import java.util.ArrayList;
 
 public class ProjectController {
 
-	// todo: fjern name og email
 	// todo: lav liste af projekter til en del af databasen.
 
-	private String name;
-	private String email;
 	private List<Project> projects;
 
 	// Constructor (forklar)
 	public ProjectController(String name, String email) {
-		this.name = name;
-		this.email = email;
 		this.projects = new ArrayList<>();
 	}
 
@@ -31,21 +26,5 @@ public class ProjectController {
 
 	public List<Project> getProjects() {
 		return projects;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 }
