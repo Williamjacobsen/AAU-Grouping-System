5ea8bdf3167a00326a1658d69d84162c0ce171d4
MunasarMohamed <mmam24@student.aau.dk>
2025-10-13 08:36:23 +0200

    Added notes

---
+++ frontend/src/pages/User/Profile.jsx
@@ -0,0 +1,7 @@
+
+// fetch user info with /me
+
+// display name, role and etc.
+
+// add modify buttons and log out button.
+
---
+++ frontend/src/pages/User/ProtectedRoute.jsx
@@ -0,0 +1,6 @@
+// GET /auth/me
+
+// if res.ok, then let the page load
+
+// else redirect to login
+
