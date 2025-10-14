5123b298c513e0ec36965f65ddc9154777fab655
MunasarMohamed <mmam24@student.aau.dk>
2025-10-14 10:28:45 +0200

    commit 1

--- frontend/src/pages/User/SignUp.jsx
+++ frontend/src/pages/User/SignUp.jsx
@@ -26,8 +26,8 @@ export default function SignUp() {
 					setError(errorMessage);
 				}
 			})
-			.catch(() => {
-				setError("Error: Check your network");
+			.catch((e) => {
+				setError(e);
 			})
 	}
 
