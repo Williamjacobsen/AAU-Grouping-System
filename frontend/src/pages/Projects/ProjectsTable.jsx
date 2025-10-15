5ff299648b959b46b3922db120b0d99e9bb14dbd
Jesp <87063030+JespJesp@users.noreply.github.com>
2025-10-15 09:51:37 +0200

    Added basic stuff and hints to help Magnus

---
+++ frontend/src/pages/Projects/ProjectsTable.jsx
@@ -0,0 +1,13 @@
+import React, { useMemo, memo } from "react";
+
+const ProjectsTable = memo(({ projects }) => {
+
+	return (
+    <table>
+			TODO: Lav en tabel over "projects"-variablen.
+    </table>
+  );
+
+});
+
+export default ProjectsTable;
