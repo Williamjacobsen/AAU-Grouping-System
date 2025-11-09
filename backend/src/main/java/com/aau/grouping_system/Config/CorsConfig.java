2a10d4651b1630dfba1202434cb46a9eb8181cad
Williamjacobsen <villi05.v.j@gmail.com>
2025-11-09 21:18:49 +0100

    BUG FIX: cors issue

--- backend/src/main/java/com/aau/grouping_system/Config/CorsConfig.java
+++ backend/src/main/java/com/aau/grouping_system/Config/CorsConfig.java
@@ -1,25 +1,26 @@
 package com.aau.grouping_system.Config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;;
 
 @Configuration
 public class CorsConfig {
 
 	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-
+		config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
-		config.addAllowedOriginPattern("http://localhost:3000");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
 
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
+		return source;
 	}
 }
