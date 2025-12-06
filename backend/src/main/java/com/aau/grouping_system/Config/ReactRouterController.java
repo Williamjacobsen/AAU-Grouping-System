49e30465bbd12858bd12a2887baadf9c59148b99
Williamjacobsen <villi05.v.j@gmail.com>
2025-12-06 12:28:35 +0100

    woops

--- backend/src/main/java/com/aau/grouping_system/Config/ReactRouterController.java
+++ backend/src/main/java/com/aau/grouping_system/Config/ReactRouterController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReactRouterController {
-
-    // This matches any path NOT starting with /api/ or /static/
-    @RequestMapping(value = "/{path:^(?!api|static).*}")
-    public String forward() {
-        return "forward:/index.html";  // Forwards to React's entry point
-    }
+	// This matches any path NOT starting with /api/, /static/, or /ws
+	@RequestMapping(value = "/{path:^(?!api/|static/|ws).*}")
+	public String forward() {
+		return "forward:/index.html"; // Forwards to React's entry point
+	}
 }
